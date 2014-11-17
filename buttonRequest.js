21b09303fdc1865e860f2ca57a0c6f25b3d9ed26
NVarma <nikita.varma@sjsu.edu>
2014-11-17 15:08:25 -0800

    initial commit

---
+++ buttonRequest.js
@@ -0,0 +1,48 @@
+var userDetails;
+var httpRequest;
+function storeData(){
+	console.log("in store data func");
+	 userDetails = {
+        firstname : document.getElementById("firstname").value,
+        lastname : document.getElementById("lastname").value,
+        email :  document.getElementById("email").value,
+        password1 : document.getElementById("password1").value,
+        dob1 : document.getElementById("dob1").value,
+        dob3 : document.getElementById("dob3").value,
+        ssn : document.getElementById("ssn").value,
+        phone_no : document.getElementById("phone_no").value,
+        creditcard : document.getElementById("creditcard").value,
+        url : document.getElementById("url").value,
+        age: document.getElementById("age").value,
+        gender: document.getElementById("gender").value
+    };
+    console.log("in store data func end ");
+    handleButtonRequest();
+    console.log("in store data func checkng vl"+JSON.stringify(userDetails));
+}
+
+function handleButtonRequest(e){
+	//e.preventDefault();
+	console.log("in handle request func");
+
+	try
+	{
+		httpRequest = new XMLHttpRequest();
+		httpRequest.onreadstatechange = handleResponse;
+		httpRequest.open("POST", "http://localhost:8800/servernew.js",true);
+		httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		httpRequest.send(JSON.stringify(userDetails));
+	}
+	catch(err)
+	{
+		console.log("Error is"+err);
+	}
+}
+function handleResponse(e){
+		console.log("in handle response func");
+
+	if(httpRequest.readyState == 4 && httpRequest.status == 200)
+	{
+		console.log(httpRequest.responseText);
+	}
+}
---
+++ servernew.js
@@ -0,0 +1,54 @@
+
+var mysql = require('mysql');
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (request,response){
+	 console.log("in create server ");
+	var formdata = "";
+	request.on("data", function(data){
+	formdata += data;
+	 console.log("in req.on func ");
+	});
+	
+	request.on('end', function(){
+		 console.log("in req.on end func1 ");
+		try {
+		var dataObj = JSON.parse(formdata);
+		var firstname = JSON.stringify(dataObj.firstname);
+		var lastname = JSON.stringify(dataObj.lastname);
+		var email = JSON.stringify(dataObj.email);
+		var password1 = JSON.stringify(dataObj.password1);
+		var dob1 = JSON.stringify(dataObj.dob1);
+		var dob3 = JSON.stringify(dataObj.dob3);
+		var ssn = JSON.stringify(dataObj.ssn);
+		var phone_no = JSON.stringify(dataObj.phone_no);
+		var creditcard = JSON.stringify(dataObj.creditcard);
+		var url = JSON.stringify(dataObj.url);
+		var age = JSON.stringify(dataObj.age);
+		var gender = JSON.stringify(dataObj.gender)
+	}
+	catch(err)
+	{
+		console.log("error with json.parse"+err);
+	}
+
+
+		function db1(){
+		var connection = mysql.createConnection( {host: 'localhost', user: 'root', password: 'root', port: 3305, database: 'test'}); return connection;}
+		var objectDataB = db1();
+		console.log("in db1 method..connection establshd ");
+
+	objectDataB.query("INSERT INTO registration1(firstname, lastname, email, password1, dob1, dob3, ssn, phone_no, creditcard, url, age, gender) VALUES ('"+firstname+"','"+lastname+"','"+email+"','"+password1+"','"+dob1+"','"+dob3+"','"+ssn+"','"+phone_no+"','"+creditcard+"','"+url+"','"+age+"','"+gender+"');", function (err){
+		objectDataB.end();
+		if(err) throw err;
+		response.writeHead(	200, {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET, POST'});
+
+		//response.end("database enrty made");
+	});
+});
+}).listen(8800);
+	console.log("server is listening to port 8888");		
